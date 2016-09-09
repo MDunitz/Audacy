0e61c9bc3cb6883b25962352c95d6cf3d0d36f73
Madison <midunitz@ucdavis.edu>
2016-09-09 12:47:17 -0700

    server running locally, add google signin/out

---
+++ server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var app = express();
+var router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: true }));
+
+app.use(express.static(__dirname+'/../public/client'));
+
+app.use('/api', router);
+// require('./router.js')(router);
+
+app.set('port', (process.env.PORT || 8000) );
+
+module.exports = app;
